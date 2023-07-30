575e507b4280377f89f2cbfc29c08d5755cae039
Jeffrey Holmes <coffeman500@gmail.com>
2023-07-30 00:48:02 -0500

    Stopped at entites

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule, 
+    ConfigModule.forRoot(),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
